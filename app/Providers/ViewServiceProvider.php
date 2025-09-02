8017a1d74b961680d95c1ba5356975bf48b922ca
Razmik Ayvazyan <79054971+ayvazyan10@users.noreply.github.com>
2025-09-03 00:35:22 +0400

    refactor layout and improve UI components across multiple views

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Page;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        View::share('page_menu_header', Page::where('position', 'top')->get());
+        View::share('page_menu_footer', Page::where('position', 'bottom')->get());
+    }
+}
