709a78a352815c92944e846995592138bde620f1
Razmik Ayvazyan <79054971+ayvazyan10@users.noreply.github.com>
2025-09-07 22:34:07 +0400

    add oEmbed support for YouTube and Vimeo in page content

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -56,6 +56,8 @@ class PageController extends Controller
     {
         $page = \App\Models\Page::where('slug', $slug)->firstOrFail();
 
+        $page->content = $this->embed_oembed_to_iframe($page->content);
+
         return view('page', compact('page'));
     }
 
@@ -66,5 +68,30 @@ class PageController extends Controller
         return view('golf', [
             'page' => $template,
         ]);
+    }
+
+
+    public function embed_oembed_to_iframe(string $html): string {
+        return preg_replace_callback(
+            '/<oembed\s+url="([^"]+)"\s*><\/oembed>/i',
+            function($m) {
+                $url = $m[1];
+
+                if (preg_match('%(?:youtu\.be/|youtube(?:-nocookie)?\.com/(?:watch\?v=|embed/|v/|shorts/))([A-Za-z0-9_-]{4,})%i', $url, $mm)) {
+                    $id = $mm[1];
+                    return '<div class="media"><iframe src="https://www.youtube.com/embed/' . htmlspecialchars($id, ENT_QUOTES, 'UTF-8')
+                        . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width:100%;height:400px;"></iframe></div>';
+                }
+
+                if (preg_match('%vimeo\.com/(?:video/)?(\d+)%i', $url, $mm)) {
+                    $id = $mm[1];
+                    return '<div class="media"><iframe src="https://player.vimeo.com/video/' . htmlspecialchars($id, ENT_QUOTES, 'UTF-8')
+                        . '" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="width:100%;height:400px;"></iframe></div>';
+                }
+
+                return $m[0];
+            },
+            $html
+        );
     }
 }
