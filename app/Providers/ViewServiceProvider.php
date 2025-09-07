b9ca5a48ffd5ed268cf0ce64229b67c1a49e17c7
Razmik Ayvazyan <79054971+ayvazyan10@users.noreply.github.com>
2025-09-07 22:24:38 +0400

    update page menu sharing to include sorting by order

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -10,7 +10,7 @@ class ViewServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        View::share('page_menu_header', Page::where('position', 'top')->get());
-        View::share('page_menu_footer', Page::where('position', 'bottom')->get());
+        View::share('page_menu_header', Page::where('position', 'top')->orderBy('sort_order')->get());
+        View::share('page_menu_footer', Page::where('position', 'bottom')->orderBy('sort_order')->get());
     }
 }
